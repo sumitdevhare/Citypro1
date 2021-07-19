f454ee12b61cd209aab44122a646a53c41432f63
sumitdevhare <80598966+sumitdevhare@users.noreply.github.com>
2021-07-19 15:49:45 +0530

    commit-sec

--- person/admin.py
+++ person/admin.py
@@ -5,7 +5,7 @@ from person.models import Taluka, City, Person,Village
 
 @admin.register(Person)
 class PersonAdmin(ImportExportModelAdmin):
-    list_display =('city','taluka','village','first_name','middal_name','last_name','phone','addhar','addhar','image')
+    list_display =('city','taluka','village','first_name','middal_name','last_name','phone','addhar','address','image')
     pass
     
 
